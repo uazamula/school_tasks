41c03d2b29226650c3afa1de9a1c6f0e851a842c
uazamula <uazamula@ukr.net>
2026-09-18 12:27:27 +0300

    v.0.13.3.0 topic multiplication
    TODO: (v. 0.13.3)
    
    Перед commit виконуємо три команди:
    dart format .
    flutter analyze
    dart run build_runner build --delete-conflicting-outputs

--- lib/features/learning/data/topics/topic_multiplication_table.dart
+++ lib/features/learning/data/topics/topic_multiplication_table.dart
@@ -1,4 +1,9 @@
 import 'package:school_tasks/features/learning/data/fixed_task_data/multiplication.dart';
+import 'package:school_tasks/features/learning/data/generation/arithmetic/arithmetic_generator_config.dart';
+import 'package:school_tasks/features/learning/data/generation/arithmetic/arithmetic_operation.dart';
+import 'package:school_tasks/features/learning/data/generation/arithmetic/arithmetic_task_data_generator.dart';
+import 'package:school_tasks/features/learning/data/generation/generated_task_data_source.dart';
+import 'package:school_tasks/features/learning/data/generation/wrong_answer_generator.dart';
 import 'package:school_tasks/features/learning/domain/evaluation/evaluation_config.dart';
 import 'package:school_tasks/features/learning/domain/evaluation/evaluation_criterion_type.dart';
 import 'package:school_tasks/features/learning/domain/evaluation/passing_criteria.dart';
@@ -11,10 +16,31 @@ abstract final class MultiplicationTable {
   static final Topic topic = Topic(
     id: 'multiplication_table',
     taskTypeCounts: {
-      LearningTaskType.selection: 2,
-      LearningTaskType.numericInput: 1,
+      // LearningTaskType.selection: 5,
+      LearningTaskType.numericInput: 10,
     },
-    dataSource: FixedTaskDataSource(MultiplicationTaskData.multiplicationTable),
+    dataSource: GeneratedTaskDataSource([
+      ArithmeticTaskDataGenerator(
+        operation: ArithmeticOperation.multiplication,
+        imageForGrid: 'assets/images/tasks/apple.png',
+        useNumericInput: true,
+        config: ArithmeticGeneratorConfig(
+          minA: 2,
+          maxA: 6,
+          minB: 3,
+          maxB: 9,
+          minimumResult: 10,
+          maximumResult: 81,
+          wrongAnswerMinimumResult: 10,
+          wrongAnswerMaximumResult: 81,
+          wrongAnswerStrategy: WrongAnswerStrategy.randomInRange,
+          wrongAnswerCount: 4,
+          divisibilityA: 1,
+          resultDivisibility: 1,
+          wrongAnswerDivisibility: 1,
+        ),
+      ),
+    ]),
     help: 'Тут буде довідка про множення.',
     evaluation: EvaluationConfig(
       weights: {
@@ -22,8 +48,8 @@ abstract final class MultiplicationTable {
         EvaluationCriterionType.time: 3,
       },
       time: TimeEvaluationConfig(
-        targetTime: Duration(seconds: 10),
-        maximumTime: Duration(seconds: 20),
+        targetTime: Duration(seconds: 20),
+        maximumTime: Duration(seconds: 40),
       ),
     ),
     passingCriteria: PassingCriteria(minimumAccuracy: 0.0),
